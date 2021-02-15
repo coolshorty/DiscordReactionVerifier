b83fda736c97de689f6f1cec3cd5ca8600f67af8
coolshorty <77104733+coolshorty@users.noreply.github.com>
2021-02-15 13:44:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    role_name = ""
-    message_id = ""
+    role_name = "verified"
+    message_id = "810838847385501696"
     prefix = "."
-    bot_token = ""
-    channel_id = ""
+    bot_token = "ODEwODM2ODExNDMzNzcxMDE4.YCpcoA.KnmTnpgMl4mAE8mvXMo-LxPqhtU"
+    channel_id = "806910868959920182"
